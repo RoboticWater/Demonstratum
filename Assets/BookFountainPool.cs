89bd05ba868508f62b0230532689e3f72b5343ba
RoboticWater <jakebritti@gmail.com>
2019-03-09 13:42:58 -0500

    added first state of BookFoutainPool

---
+++ Assets/BookFountainPool.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookFountainPool : ChordListener
+{
+    public WaterSpout[] spouts;
+    public WaterRise water;
+    public float riseDelay = 0.3f;
+
+    private void OnTriggerEnter(Collider other) {
+        StartCoroutine(StartWater());
+    }
+
+    IEnumerator StartWater() {
+        foreach (WaterSpout s in spouts) {
+            s.StartSpout();
+        }
+        yield return new WaitForSeconds(riseDelay);
+        water.StartRise();
+    }
+}
