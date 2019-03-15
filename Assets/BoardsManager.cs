b42f47892ae04dc34e5bd32ec68ac4f9be5f8742
RoboticWater <jakebritti@gmail.com>
2019-03-15 01:31:07 -0400

    added furnace +wood selector

--- Assets/BoardsManager.cs
+++ Assets/BoardsManager.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class BoardsManager : ChordListener
 {
     public Wood[] wood;
+    public GameObject woodSelectorPrefab;
     private void OnTriggerEnter(Collider other) {
+        Instantiate(woodSelectorPrefab, wood[0].transform);
         foreach (Wood w in wood) {
             w.Break();
         }
--- Assets/FurnaceFire.cs
+++ Assets/FurnaceFire.cs
@@ -10,11 +10,27 @@ public class FurnaceFire : MonoBehaviour
     public float fireStartSize = 0.1f;
     public float fireEndSize = 0.1f;
 
+    public float lightRandom = 0.1f;
+    public float lightSpeed = 0.1f;
+    public float minLightIntensity = 0.9f;
+    public float maxLightIntensity = 1;
+    public float minAreaSize = 2.5f;
+    public float maxAreaSize = 8.15f;
+    float intensityTarget = 1;
+
     ParticleSystem fire;
+    public Light light;
 
     private void Start()
     {
         fire = GetComponent<ParticleSystem>();
+    }
+
+    private void FixedUpdate() {
+        light.intensity = Mathf.Lerp(light.intensity, intensityTarget, lightSpeed);
+        if (Random.Range(0.0f, 1.0f) < lightRandom) {
+            light.intensity = Random.Range(0.8f, 1); 
+        }
     }
 
     private void OnTriggerEnter(Collider other) {
@@ -23,6 +39,7 @@ public class FurnaceFire : MonoBehaviour
             curWood += 1;
             float fireSize = Mathf.Lerp(fireStartSize, fireEndSize, (float) totalWood / curWood);
             fire.transform.localScale = new Vector3(fireSize, fireSize, fireSize);
+            light.range = Mathf.Lerp(minAreaSize, maxAreaSize, (float) totalWood / curWood);
             w.Die();
         }
     }
