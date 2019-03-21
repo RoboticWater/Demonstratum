0432aae2d93b6c0ac810bfc4ada237a203106d17
RoboticWater <jakebritti@gmail.com>
2019-03-21 02:07:22 -0400

    waterfalls with rendertextures

---
+++ Assets/SetInteractiveShaderEffects.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class SetInteractiveShaderEffects : MonoBehaviour
+{
+    [SerializeField]
+    RenderTexture rt;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Shader.SetGlobalTexture("_OcclusionTex", rt);
+        Shader.SetGlobalFloat("_OrthographicCamSize", GetComponent<Camera>().orthographicSize);
+    }
+ 
+}
