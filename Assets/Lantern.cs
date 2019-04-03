eb38425b9711109c078ffb6de39b142047846a88
RoboticWater <jakebritti@gmail.com>
2019-04-03 02:21:49 -0400

    added git large file system +lamp shader + forest foliage

---
+++ Assets/Lantern.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lantern : MonoBehaviour
+{
+    Material m;
+
+    bool lighting;
+    bool lit;
+
+    private void Awake() {
+        m = GetComponent<Renderer>().material;
+    }
+
+    private void OnDestroy() {
+        Destroy(m);
+    }
+
+    public ToggleLight() {
+        if (!lighting)
+            StartCoroutine(ToggleLightRoutine());
+    }
+
+    IEnumerator ToggleLightRoutine() {
+
+    }
+}
