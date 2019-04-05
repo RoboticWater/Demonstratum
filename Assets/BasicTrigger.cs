40fe002ea61e3ab923f6b7573359ad7a714d9732
RoboticWater <jakebritti@gmail.com>
2019-04-05 03:01:35 -0400

    wood collapse trigger + corrected rosetta vines uv and rosetta room uvs

---
+++ Assets/BasicTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BasicTrigger : MonoBehaviour
+{
+    public UnityEvent e;
+    private void OnTriggerEnter(Collider other) {
+        e.Invoke();
+    }
+}
